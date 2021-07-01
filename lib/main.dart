9e97c55e4a629766e4202eb8b2c28048ce26b828
Rune Langøy <rune.langoy@gmail.com>
2021-07-01 13:27:55 +0200

    Test ViewPage

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'screens/config_server/config_server_screen.dart';
 import 'screens/errors/no_connection.dart';
 import 'screens/errors/unauthorized.dart';
+import 'screens/messages/messages.dart';
 import 'screens/login/login_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:mqtt_client/mqtt_client.dart';
@@ -181,7 +182,14 @@ Future<void> main() async {
 
 // ignore: must_be_immutable
 class MyApp extends StatelessWidget {
-  MyApp({Key? key, this.loginInfo}) : super(key: key);
+  MyApp({Key? key, this.loginInfo}) : super(key: key) {
+    _lstItems = List<ListItem>.generate(
+      1000,
+      (i) => i % 6 == 0
+          ? HeadingItem('Heading $i')
+          : MessageItem('Sender $i', 'Message body $i'),
+    );
+  }
   MqttLoginInfo? loginInfo;
   MqClient? mqClient;
 
@@ -191,30 +199,32 @@ class MyApp extends StatelessWidget {
     mqClient!.connect(context);
   }
 
+  List<ListItem>? _lstItems;
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      //ConfigServerScreen
-      //home: ConfigServerScreen(loginInfo: loginInfo, onLoginBtn: onLoginBtn),
-      //home: const NoConnectionScreen(msgHeader: 'Sorry!..'),
-      //home: UnAuthorized(msgHeader: 'Access Denied'),
-      home: LoginScreen(loginInfo: loginInfo, onLoginBtn: onLoginBtn),
-      //const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        //ConfigServerScreen
+        //home: ConfigServerScreen(loginInfo: loginInfo, onLoginBtn: onLoginBtn),
+        //home: const NoConnectionScreen(msgHeader: 'Sorry!..'),
+        //home: UnAuthorized(msgHeader: 'Access Denied'),
+        home: MqttMessageList(items: _lstItems!)
+        //home: LoginScreen(loginInfo: loginInfo, onLoginBtn: onLoginBtn),
+        //const MyHomePage(title: 'Flutter Demo Home Page'),
+        );
   }
 }
 
---
+++ lib/screens/messages/messages.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class MqttMessageList extends StatefulWidget {
+  List<ListItem>? items;
+
+  MqttMessageList({Key? key, List<ListItem>? items}) : super(key: key) {
+    this.items = items;
+  }
+
+  @override
+  State<StatefulWidget> createState() => _MqttMessageList(items: items!);
+}
+
+class _MqttMessageList extends State<MqttMessageList> {
+  List<ListItem>? items;
+  _MqttMessageList({List<ListItem>? items}) {
+    this.items = items;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Mixed List';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: ListView.builder(
+          // Let the ListView know how many items it needs to build.
+          itemCount: items!.length,
+          // Provide a builder function. This is where the magic happens.
+          // Convert each item into a widget based on the type of item it is.
+          itemBuilder: (context, index) {
+            final item = items![index];
+
+            return ListTile(
+              title: item.buildTitle(context),
+              subtitle: item.buildSubtitle(context),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+/// The base class for the different types of items the list can contain.
+abstract class ListItem {
+  /// The title line to show in a list item.
+  Widget buildTitle(BuildContext context);
+
+  /// The subtitle line, if any, to show in a list item.
+  Widget buildSubtitle(BuildContext context);
+}
+
+/// A ListItem that contains data to display a heading.
+class HeadingItem implements ListItem {
+  final String heading;
+
+  HeadingItem(this.heading);
+
+  @override
+  Widget buildTitle(BuildContext context) {
+    return Text(
+      heading,
+      style: Theme.of(context).textTheme.headline5,
+    );
+  }
+
+  @override
+  Widget buildSubtitle(BuildContext context) => SizedBox();
+}
+
+/// A ListItem that contains data to display a message.
+class MessageItem implements ListItem {
+  final String sender;
+  final String body;
+
+  MessageItem(this.sender, this.body);
+
+  @override
+  Widget buildTitle(BuildContext context) => Text(sender);
+
+  @override
+  Widget buildSubtitle(BuildContext context) => Text(body);
+}
